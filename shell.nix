7c7dc63ba99d63caab00c26fcd01cfe22bc812d4
Thomas Heck <t@b128.net>
2020-03-07 10:17:34 +0100

    add missing dep

--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ in
             rustpkgs.rls-preview
             rustpkgs.rustfmt-preview
             gst_all_1.gstreamer
-            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-base # needed for opus, resample, ...
+            gst_all_1.gst-plugins-good # needed for flac
         ];
     }
